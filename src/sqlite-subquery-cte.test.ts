9e22b521f0c00f48520a37e4d05e305c6ffd3e5b
GyuJae <wjdrbwo1206@naver.com>
2024-11-24 21:36:50 +0900

    test: CTEs

--- src/sqlite-subquery-cte.test.ts
+++ src/sqlite-subquery-cte.test.ts
@@ -9,6 +9,7 @@ describe('Subqueries and CTEs', () => {
   /**
    * SELECT * FROM movies WHERE rating > (SELECT AVG(rating) FROM movies);
    */
+  // https://orm.drizzle.team/docs/select#select-from-subquery
   test('Independent Subqueries: 전체 영화들 중, 평점이 평균보다 높은 영화의 리스트', async () => {
     const result = await db
       .select()
@@ -16,5 +17,41 @@ describe('Subqueries and CTEs', () => {
       .where(gt(movies.rating, sql<number>`(SELECT AVG(rating) FROM movies)`));
 
     expect(result.length).toBe(99_501);
+  });
+
+  /*
+    WITH avg_revenue AS (
+        SELECT AVG(revenue) AS value FROM movies
+    )
+    SELECT 
+        movies.title,
+        movies.director,
+        movies.revenue,
+        (SELECT value FROM avg_revenue) AS avgRevenue
+    FROM movies
+    WHERE movies.revenue > (SELECT value FROM avg_revenue);
+   */
+  // https://orm.drizzle.team/docs/update#with-update-clause
+  test('CTEs(Common Table Expression)', async () => {
+    const avgRevenue = db.$with('avg_revenue').as(
+      db
+        .select({
+          value: sql<number>`AVG(revenue)`.as('value'),
+        })
+        .from(movies),
+    );
+
+    const result = await db
+      .with(avgRevenue)
+      .select({
+        title: movies.title,
+        director: movies.director,
+        revenue: movies.revenue,
+        avgRevenue: sql<number>`(SELECT value FROM avg_revenue)`,
+      })
+      .from(movies)
+      .where(gt(movies.revenue, sql<number>`(SELECT value FROM avg_revenue)`));
+
+    expect(result.length).toBe(2761);
   });
 });
